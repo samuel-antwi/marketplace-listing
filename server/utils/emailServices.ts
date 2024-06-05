0d2f7e3a5d898a9e5b53ab875d5ea24d78c46f41
Samuel Antwi <samuelantwidev@gmail.com>
2024-06-05 08:52:16 +0100

    Update email verification message with user's name and improve formatting

--- server/utils/emailServices.ts
+++ server/utils/emailServices.ts
@@ -14,11 +14,41 @@ export async function sendVerificationEmail(
   const msg = {
     to: email,
     from: "samuelantwidev@gmail.com",
-    subject: "Email Verification",
-    text: `Your verification code is: ${code}`,
+    subject: "Verify Your Email Address for Onegii Ltd",
+    text: `
+Dear ${user.given_name},
+
+Thank you for signing up with Onegii We are excited to have you on board.
+
+To complete your registration and activate your account, please use the verification code below:
+
+Verification code: ${code}
+
+If you did not create an account with Onegii Ltd, please disregard this email.
+
+If you have any questions or need assistance, feel free to contact our support team at support@onegii.com.
+
+Thank you for choosing Onegii Ltd. We look forward to serving you.
+
+Best regards,
+
+<p><strong>Samuel Antwi</strong><br>
+<strong>Customer Support</strong><br>
+<strong>Onegii Ltd</strong><br>
+    `,
     html: `
-    <p>Hello <strong>${user.given_name}, </strong>it is greate to have you onboard. Below is your verification code</p>
-    <p>Verification code: <strong>${code}</strong></p>
+<p>Dear <strong>${user.given_name}</strong>,</p>
+<p>Thank you for signing up with <strong>Onegii</strong>! We are excited to have you on board.</p>
+<p>To complete your registration and activate your account, please use the verification code below:</p>
+<p><strong>Verification code: ${code}</strong></p>
+<p>If you did not create an account with <strong>Onegii Ltd</strong>, please disregard this email.</p>
+<p>If you have any questions or need assistance, feel free to contact our support team at <strong>support@onegii.com</strong>.</p>
+<p>Thank you for choosing <strong>Onegii</strong>. We look forward to serving you.</p>
+<br>
+<p>Best regards,</p>
+<p><strong>Samuel Antwi</strong><br>
+<strong>Customer Support</strong><br>
+<strong>Onegii Ltd</strong><br>
     `,
   };
 
